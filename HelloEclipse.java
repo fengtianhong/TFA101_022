70179ca2fbfb2a350d2500ca14289e0f1aa0c7c6
HakunaMatata <4a090136@stust.edu.tw>
2021-05-16 14:03:05 +0800

    first version

---
+++ HelloEclipse.java
@@ -0,0 +1,17 @@
+package xxx;
+
+public class HelloEclipse {
+	public static void main(String[] args) {
+		int test = 1_234_543;
+		String a = new String(" aaa ");
+		a= " ";
+		String c = a.trim();
+		String e = a.trim();
+		String b = "";
+		String d = "";
+		System.out.println(c==e);
+		System.out.print(a);
+		
+		
+	}
+}
