eecedd9a41b24013565779ec84c25fafca60b183
HakunaMatata <4a090136@stust.edu.tw>
2021-05-16 14:30:48 +0800

    3

--- HelloEclipse.java
+++ HelloEclipse.java
@@ -3,6 +3,7 @@ package xxx;
 public class HelloEclipse {
 	public static void main(String[] args) {
 		System.out.print("sayMyName")
+		System.out.print("sayMyNameAgain")
 		int test = 1_234_543;
 		String a = new String(" aaa ");
 		a= " ";
